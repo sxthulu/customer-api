3423f53d06f9965140dea1b9bf901ef0e7e07f00
SHIVA RAI <s.xthulu@gmail.com>
2026-09-07 21:58:45 -0400

    Setter removed from Response DTO

--- src/main/java/com/example/customerapi/dto/CustomerResponse.java
+++ src/main/java/com/example/customerapi/dto/CustomerResponse.java
@@ -20,22 +20,6 @@ public class CustomerResponse {
         this.email = email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Long getId() {
         return id;
     }
